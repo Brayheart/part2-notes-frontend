a6947c6202c7be88e6f4847b217268c929d9fc86
Tyler Bray <tylerhbray@gmail.com>
2023-03-27 12:00:00 -0700

    notes playground

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
+      {/* test? */}
       <ul>
         {notes.map((note) => (
           <li key={note.id}>{note.content}</li>
