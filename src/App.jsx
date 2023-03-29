ed82b52bcfae53652c19dcc148ec40aa56ee7d94
Tyler Bray <tylerhbray@gmail.com>
2023-03-29 12:00:00 -0700

    notes playground

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import Note from './components/Note'
-
 const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note} />
-        )}
+        {notes.map((note) => (
+          <li key={note.id}>{note.content}</li>
+        ))}
       </ul>
     </div>
-  )
-}
-
-export default App
+  );
+};
+export default App;
